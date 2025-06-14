aa2cfd9480d746584548f889b54ad473645a264e
chrisd0314 <chihiro603@gmail.com>
2025-06-14 13:41:10 +0900

    Add files via upload

---
+++ kadai_011/foreach.php
@@ -0,0 +1,12 @@
+<?php
+    $vegetables = [
+        'name' : '玉ねぎ',
+        'price' : 200,
+        'productarea' : '北海道'
+    ];
+
+    foreach($vegetables as $key => $value){
+        echo $key : $value '<br>';
+    }
+
+?>
