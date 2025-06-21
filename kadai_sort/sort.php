0f06d498803579ed5c67fa505a898a8b51e75c2b
chrisd0314 <chihiro603@gmail.com>
2025-06-21 21:16:03 +0900

    Add files via upload

--- kadai_sort/sort.php
+++ kadai_sort/sort.php
@@ -12,21 +12,21 @@
         // ソートする配列を宣言
         $nums = [15, 4, 18, 23, 10 ];
 
-        function sort_2way($nums,true){
-            if ($order === true){
+        function sort_2way($array, $order){
+            if ($order){
             echo "昇順にソートします。<br>";
             sort($array);
-            foreach ($array as $num) {
-            echo "{$num}<br>";
-            }
         } else {
             echo "降順にソートします。<br>";
             rsort($array); 
-            foreach ($array as $num) {
-            echo "{$num}<br>";
-            }  
         }
-    }
+        foreach ($array as $num) {
+            echo $num . "<br>";
+            }
+        }
+        
+        sort_2way($nums,true);
+        sort_2way($nums,false);
         ?>
     </p>
 </body>
